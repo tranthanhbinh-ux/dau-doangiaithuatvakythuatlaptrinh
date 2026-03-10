ae7b2399d5c592ba843ba683565bf0ddb3b31dbf
tranthanhbinh-ux <binsso0937821350@gmail.com>
2026-03-10 10:29:29 +0700

    Add files via upload

---
+++ Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        for num in nums:
+            index = abs(num) - 1
+            nums[index] = -abs(nums[index])
+        result = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i + 1)
+                
+        return result
