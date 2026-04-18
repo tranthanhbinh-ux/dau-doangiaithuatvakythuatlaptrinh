059eaa03f6729909d315bcbc862451e1c5282c53
tranthanhbinh-ux <binsso0937821350@gmail.com>
2026-04-19 00:20:40 +0700

    Add files via upload

---
+++ 2215. Find the Difference of Two Arrays.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        return [list(set1 - set2), list(set2 - set1)] #hieu 2 phan nums
